5cb7e6cc0c937cd1f0f63bd4ad7addb72ae27acc
RachanaDontula <rachanadontula3@gmail.com>
2019-09-08 17:01:03 +0800

    notes1

---
+++ DAY-2/notes1.py
@@ -0,0 +1,56 @@
+"""Python MySQL Database Access:
+*******************************
+
+The Python standard for database interfaces is the Python BD-API.
+Most Python database interfaces adhere to this standard.
+
+We can chose the right database for the application.
+Python Database A{PI supports a wide range of databases such as:
+
+1. GadFly
+2. mSQL
+3. MySQL
+4. PostgreSQL
+5. Microsoft SQL Server 2000
+6. Informix
+7. Interbase
+8. Oracle
+9. Sybase
+10. mariadb
+
+
+Ways of Python:
+
+1. Interactive :
+2. Scripting :
+
+C language
+
+file : a.c
+
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
+code ----> compiler -----> Machine Code : 0101010101001010
+
+Windows|MacBook|Linux & CPU : Output : Hello, World!
+
+File Name : OurFirstJavaCode.java
+
+class A {
+ public static void main(String args[]){
+     System.out.println("Hello World");
+ }
+}
+
+javac OurFirstJavaCode.java
+
+Java ---> Compiler --->> Java ByteCode ---> JVM(Java Virtual Machine) ---> Machine Code:0101010101001010
+
+Windows|MacBook|Linux & CPU : Hello World
+"""
